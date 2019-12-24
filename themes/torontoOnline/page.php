e4783323023e34d4853536c0a08b29d9d8d5c16f
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-23 16:53:31 -0800

    Create page.php and copy over code from index..php

--- themes/torontoOnline/page.php
+++ themes/torontoOnline/page.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 
-<div id="primary" class="primary">
+<div id="primary" class="primary post-<?php the_ID(); ?>">
     <?php while(have_posts() ): the_post(); ?>
         <h1><?php the_title(); ?></h1>
 
         <?php the_content(); ?>
 
-        <h2>From index.php!</h2>
+        <h2>From page.php!</h2>
 
 <?php endwhile; ?>
 
