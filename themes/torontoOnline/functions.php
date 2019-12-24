25586bda85a45a82e371f8b05a132c85af497923
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-23 17:28:26 -0800

    Create functions file. Enqueue scripts, link styles. Test CSS with body background color

---
+++ themes/torontoOnline/functions.php
@@ -0,0 +1,12 @@
+<?php 
+
+// Register Styles and Scripts
+
+function torontoOnline_scripts() {
+    wp_enqueue_style('style', get_stylesheet_uri() );
+
+
+}
+add_action('wp_enqueue_scripts', 'torontoOnline_scripts');
+
+?>
