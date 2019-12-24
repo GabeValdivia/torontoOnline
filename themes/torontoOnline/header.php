b4cb5b8ff20cad81a0a73d172f458672917c68d8
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-23 17:18:49 -0800

    Create img directory and add needed assets. Writh php code in header template needed to pull in logo svg and add link to homepage

--- themes/torontoOnline/header.php
+++ themes/torontoOnline/header.php
@@ -9,6 +9,15 @@
 <body>
 
 <div id="page">
+    <header id="masthead" class="site-header" role="banner">
+        <div class="container">
+            <div class="logo">
+                <a href="<?php echo esc_url( home_url('/') ); ?>">
+                    <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg" alt="torontoOline Logo">
+                </a>
+            </div><!-- .logo -->
+        </div><!-- .container -->
+    </header>
     <div class="container content">
     
     </div>
