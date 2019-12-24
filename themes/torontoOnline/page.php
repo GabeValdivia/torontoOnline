ee1535f1e357dbf1525a223bc152937d683db2ba
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-23 19:00:36 -0800

    Create sidebar.php file, add get_sidebar() function to both index.php and page.php

--- themes/torontoOnline/page.php
+++ themes/torontoOnline/page.php
@@ -2,12 +2,14 @@
 
 <div id="primary" class="primary post-<?php the_ID(); ?>">
     <?php while(have_posts() ): the_post(); ?>
-        <h1><?php the_title(); ?></h1>
+        <h2><?php the_title(); ?></h2>
 
         <?php the_content(); ?>
 
 <?php endwhile; ?>
 
 </div>
+
+<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
---
+++ themes/torontoOnline/sidebar.php
@@ -0,0 +1,7 @@
+<div id="secondary" class="secondary">
+    <h1>Hello from Sidebar.php</h1>
+</div>
+
+<div class="clear">
+
+</div>
