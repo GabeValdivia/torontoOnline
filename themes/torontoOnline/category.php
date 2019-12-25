6417e308b6d8595ddac394c58a16e85da57387ee
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-24 20:02:04 -0800

    Finish styles for category.php template. Add link back to single post on image and excerpt

--- themes/torontoOnline/category.php
+++ themes/torontoOnline/category.php
@@ -1,13 +1,21 @@
 <?php get_header(); ?>
 
+<h1><?php _e('Category: ', 'torontoOnline') . single_cat_title( ); ?></h1>
 
 <div id="primary" class="primary post-<?php the_ID(); ?>">
     <?php while(have_posts() ): the_post(); ?>
         
         <article>
+            <a href="<?php the_permalink(); ?>">
             <?php the_post_thumbnail('medium-blog'); ?>
-            <h2><?php the_title(); ?></h2>
-            <?php the_excerpt(); ?>
+            </a>
+            <div class="content-post">
+                <h2><?php the_title(); ?></h2>
+                <a href="<?php the_permalink(); ?>">
+                <?php the_excerpt(); ?>
+                </a>
+            </div>
+            
         </article>
 
         
