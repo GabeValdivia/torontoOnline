3779dd89b2f303049520bc7b5a6acbc0e3171f19
Gabe Valdivia <gabe@gabeswebcoding.com>
2019-12-24 23:45:51 -0800

    Pull in about us page via WP_Query. Style aAbout Us section

--- themes/torontoOnline/front-page.php
+++ themes/torontoOnline/front-page.php
@@ -2,6 +2,19 @@
 
 
 <div class="bottom-front-page">
+    <!-- About Us Section -->
+    <div class="about-us">
+        <?php $aboutUs = new WP_Query( 'page_id=12'); ?>
+        <?php while($aboutUs->have_posts() ): $aboutUs->the_post(); ?>
+
+                <h2><?php the_title(); ?></h2>
+                <?php the_content(); ?>
+                
+
+                
+        <?php endwhile; wp_reset_postdata(); ?>
+    </div>
+    <!-- Blog Tips Recent Posts -->
     <div class="blog-tips">
         <h2>Tips for Travel to Toronto</h2>
         <?php $args = array(
@@ -20,7 +33,7 @@
                 </a>
                 <h3><?php the_title(); ?></h3>
                 <?php the_excerpt(); ?>
-                
+
                 <a class="read-more" href="<?php the_permalink(); ?>">Continue Reading</a>
 
             </li>
